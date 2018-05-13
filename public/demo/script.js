214bb330c3620326e5b671b168ea4b1b55a710e3
Linus Schwab <linus.schwab@devedis.ch>
2018-05-13 11:55:08 +0200

    Visualize hue color lamp in demo

--- public/demo/script.js
+++ public/demo/script.js
@@ -1,8 +1,19 @@
 const myStrom = new MyStromSwitch(onChangeMyStrom);
+const hueLamp = new HueColorLamp(onChangeHueLamp);
 
 
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector("#mystrom-switch").addEventListener('click', () => myStrom.toggle());
+
+    document.querySelector("#hue-color-lamp .click-toggle").addEventListener('click', () => hueLamp.toggle());
+
+    const color = document.querySelector("#hue-color-lamp .color");
+    color.addEventListener('change', () => {
+        if (!hueLamp.on) {
+            hueLamp.toggle();
+        }
+        hueLamp.setColor(color.value);
+    });
 });
 
 function onChangeMyStrom(type, value) {
@@ -10,5 +21,13 @@ function onChangeMyStrom(type, value) {
         document.querySelector("#mystrom-switch .power").textContent = (value === 0 ? '' : value);
     } else if (type === 'relay') {
         document.querySelector("#mystrom-switch .relay").style.background = (value ? '#76FF03' : '#d50000');
+    }
+}
+
+function onChangeHueLamp(type, value) {
+    if (type === 'on') {
+        document.querySelector("#hue-color-lamp .color").value = (value && hueLamp.color !== undefined ? hueLamp.color : '#000000');
+    } else if (type === 'color' && hueLamp.on === true) {
+        document.querySelector("#hue-color-lamp .color").value = value;
     }
 }
--- public/demo/things.js
+++ public/demo/things.js
@@ -1,23 +1,23 @@
-const base = 'ws://localhost:5000';
+class Thing {
+    constructor(onChange, name) {
+        this.onChange = onChange;
+        this.ws = new WebSocket('ws://localhost:5000/' + name);
+    }
+}
 
-class MyStromSwitch {
+class MyStromSwitch extends Thing {
     constructor(onChange) {
-        this.ws = new WebSocket(base + '/mystrom-switch');
+        super(onChange, 'mystrom-switch');
+
         this.ws.onmessage = (event) => {
             const message = JSON.parse(event.data);
 
             if (message.hasOwnProperty('messageType') && message.messageType === 'event') {
-                const power = Math.round(message.data.change.power * 100) / 100;
-                if (this.power !== power) {
-                    this.power = power;
-                    onChange('power', power);
-                }
-
-                const relay = message.data.change.relay;
-                if (this.relay !== relay) {
-                    this.relay = relay;
-                    onChange('relay', relay);
-                }
+                this.updatePower(Math.round(message.data.change.power * 100) / 100);
+                this.updateRelay(message.data.change.relay);
+            } else if (message.hasOwnProperty('messageType') && message.messageType === 'propertyStatus') {
+                this.updatePower(Math.round(message.data.report.power * 100) / 100);
+                this.updateRelay(message.data.report.relay);
             } else {
                 console.log(message);
             }
@@ -28,13 +28,99 @@ class MyStromSwitch {
                 'messageType': 'addSubscription',
                 'data': {'event': 'change'}
             }));
+
+            this.ws.send(JSON.stringify({
+                'messageType': 'getProperty',
+                'data': {'report': {}}
+            }));
         };
+    }
+
+    updatePower(power) {
+        if (this.power !== power) {
+            this.power = power;
+            this.onChange('power', power);
+        }
+    }
+
+    updateRelay(relay) {
+        if (this.relay !== relay) {
+            this.relay = relay;
+            this.onChange('relay', relay);
+        }
     }
 
     toggle() {
         this.ws.send(JSON.stringify({
             'messageType': 'requestAction',
             'data': {'toggle': {}}
+        }));
+    }
+}
+
+class HueColorLamp extends Thing {
+    constructor(onChange) {
+        super(onChange, 'hue-color-lamp');
+
+        this.ws.onmessage = (event) => {
+            const message = JSON.parse(event.data);
+
+            if (message.hasOwnProperty('messageType') && message.messageType === 'propertyStatus') {
+                if (message.data.hasOwnProperty('on')) {
+                    this.updateOn(message.data.on);
+                } else if (message.data.hasOwnProperty('color')) {
+                    this.updateColor(message.data.color);
+                } else {
+                    console.log(message);
+                }
+            } else {
+                console.log(message);
+            }
+        };
+
+        this.ws.onopen = () => {
+            this.ws.send(JSON.stringify({
+                'messageType': 'addSubscription',
+                'data': {'property': ['on', 'color']}
+            }));
+
+            this.ws.send(JSON.stringify({
+                'messageType': 'getProperty',
+                'data': {'on': {}, 'color': {}}
+            }));
+        };
+    }
+
+    updateOn(on) {
+        if (this.on !== on) {
+            this.on = on;
+            this.onChange('on', on);
+        }
+    }
+
+    updateColor(color) {
+        if (this.color !== color) {
+            this.color = color;
+            this.onChange('color', color);
+        }
+    }
+
+    toggle() {
+        let data = true;
+        if (this.on === true) {
+            data = false;
+        }
+
+        this.ws.send(JSON.stringify({
+            'messageType': 'setProperty',
+            'data': {'on': data}
+        }));
+    }
+
+    setColor(color) {
+        this.ws.send(JSON.stringify({
+            'messageType': 'setProperty',
+            'data': {'color': color}
         }));
     }
 }
